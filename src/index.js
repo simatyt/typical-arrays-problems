5575195edfe859937dce87fa2ce09b39f7899440
Kostantin Simonenko <kostantin_simonenko@Konstansins-MacBook-Pro.local>
2020-10-03 10:31:01 +0300

    feat: implement functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (!array || array.length == 0) return 0;
+
+  return array.reduce((a, x) => x < a ? x : a, array[0]);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (!array || array.length == 0) return 0;
+
+  return array.reduce((a, x) => x > a ? x : a, array[0]);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (!array || array.length == 0) return 0;
+
+  return array.reduce((a, x) => a + x, 0) / array.length;
 }
